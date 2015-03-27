b0fe8ae5585e033f7f3686b25680281f69c429bd
Karl Johan Andreasson <kalleand@kth.se>
2015-03-27 11:28:12 +0100

    La till logback

--- web/src/main/java/se/inera/monitoring/springconfig/ApplicationConfig.java
+++ web/src/main/java/se/inera/monitoring/springconfig/ApplicationConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,7 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @Configuration
 @ComponentScan("se.inera.monitoring")
 @Import({ WebConfig.class, PersistenceConfig.class, WsConfig.class })
-@PropertySource(value = "classpath:application.properties")
+@PropertySources(@PropertySource(value = "classpath:application.properties"))
 @EnableAsync
 @EnableScheduling
 public class ApplicationConfig {
