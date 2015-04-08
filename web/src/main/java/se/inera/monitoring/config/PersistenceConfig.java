4013271b4ffa271d7fc8342af1f2d1531287cb2b
Karl Johan Andreasson <kalleand@kth.se>
2015-04-08 17:30:30 +0200

    Fixat Transport-client
    Just nu är det en lokal instans på port 9300 (standard)

--- web/src/main/java/se/inera/monitoring/config/PersistenceConfig.java
+++ web/src/main/java/se/inera/monitoring/config/PersistenceConfig.java
@@ -1,6 +1,10 @@
 package se.inera.monitoring.config;
 
-import org.elasticsearch.node.NodeBuilder;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,8 +17,20 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @EnableElasticsearchRepositories(basePackages = "se.inera.monitoring.persistence")
 public class PersistenceConfig {
 
+    @Value("${elasticsearch.url}")
+    private String elasticsearchUrl;
+    @Value("${elasticsearch.port}")
+    private int elasticsearchPort;
     @Bean
-    public ElasticsearchOperations elasticsearchTemplate() {
-        return new ElasticsearchTemplate(NodeBuilder.nodeBuilder().local(true).node().client());
+    public ElasticsearchOperations elasticsearchTemplate(Client client) {
+        return new ElasticsearchTemplate(client);
+    }
+
+    @Bean
+    public Client client() {
+        TransportClient client = new TransportClient();
+        TransportAddress address = new InetSocketTransportAddress(elasticsearchUrl, elasticsearchPort);
+        client.addTransportAddress(address);
+        return client;
     }
 }
